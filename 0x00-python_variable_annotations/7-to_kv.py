3e5bf5c7d6fadb715a29fdfaafa3501e56f5086f
derrickkimtai <derrickmoio92@gmail.com>
2024-03-14 20:40:03 +0300

    7

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
+"""returns tupel"""
 from typing import Union, Tuple
+"""returns tupel"""
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """retruns a tupel"""
     square = (v ** 2)
     result = (k, square)
 
