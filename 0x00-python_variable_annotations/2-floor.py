1ed469e8dc56fc280a06b8b6f6b8ba66860eef3d
derrickkimtai <derrickmoio92@gmail.com>
2024-03-14 13:01:07 +0300

    floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" floor function in type-  antonation"""
+import math
+
+
+def floor(n: float) -> int:
+    """takes in a float and retuns the floor of a float """
+    result = math.floor(n)
+    return result
