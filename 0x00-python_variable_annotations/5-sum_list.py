3efe79477208e443074ee883feead06b14897f3f
derrickkimtai <derrickmoio92@gmail.com>
2024-03-14 19:27:35 +0300

    mypy fix

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env ptython3
+"""sum of input list"""
+import math
+
+
+def sum_list(input_list: float) -> float:
+    """it takes floats and sums up"""
+    result = sum(input_list)
+    return result
