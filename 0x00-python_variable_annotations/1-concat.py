c8f9248963790bab5beecf81fe3f04a0e9c2636b
derrickkimtai <derrickmoio92@gmail.com>
2024-03-14 12:37:12 +0300

    concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""it returns a concatinated string """
+
+
+def concat(str1: str, str2: str) -> str:
+    """takes two strings and returns a conacatinated string"""
+    result = str1 + str2
+    return result
