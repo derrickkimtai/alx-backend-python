986cb069d37930b04fa6f351a9ff09dee126b322
derrickkimtai <derrickmoio92@gmail.com>
2024-03-15 00:43:18 +0300

    8

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -2,12 +2,12 @@
 
 """sum up a list containg integers and floats"""
 from collections.abc import Callable
-"""imports Callable"""
+"""imports Callable and it should be captal"""
 
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """return a function for multipliying a number"""
     def multiply(number: float) -> float:
-        """multiplies"""
+        """multiplies the number and the multiplier"""
         return number * multiplier
     return multiply
