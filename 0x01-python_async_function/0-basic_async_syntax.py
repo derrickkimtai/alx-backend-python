a35aa6a40b1566b3d1aa5a4e590a269819990196
derrickkimtai <derrickmoio92@gmail.com>
2024-03-18 20:16:59 +0300

    int

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -6,7 +6,7 @@ import asyncio
 import random
 
 
-async def wait_random(max_delay: float = 10) -> float:
+async def wait_random(max_delay: int = 10) -> float:
     """
     Asynchronous function
     Returns:float: The random number that was generated.
